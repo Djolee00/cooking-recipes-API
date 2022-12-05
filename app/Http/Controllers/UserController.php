34f2aabd73e60bfca2515d6cbbeb7f26569dfa49
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2022-12-05 22:40:08 +0100

    User controller finished

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $this->validate($request, [
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $th) {
+            return response()->json("All fields must be filled, bad email or user with that email exists", 400);
+        }
+        $userData = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ];
+        $newUser = User::create($userData);
+
+        return response()->json($newUser, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        if (is_null($user)) {
+            return response()->json("User with id $id not found", 404);
+        }
+
+        return response()->json($user);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $tempUser = User::find($id);
+        if (is_null($tempUser)) {
+            return response()->json("User with id $id doesn't exist.");
+        }
+
+        try {
+            if ($request->has('email') && $request->email != $tempUser->email)
+                $this->validate($request, [
+                    'email' => 'email|unique:users',
+                ]);
+        } catch (\Illuminate\Validation\ValidationException $th) {
+            return response()->json("Bad email or email exists already", 400);
+        }
+
+        if ($request->has('name'))
+            $tempUser->name = $request->name;
+        if ($request->has('email'))
+            $tempUser->email = $request->email;
+        if ($request->has('password'))
+            $tempUser->password = bcrypt($request->password);
+
+        $tempUser->update();
+
+        return response()->json($tempUser, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $tempUser = User::find($id);
+        if (is_null($tempUser)) {
+            return response()->json("User with id $id doesn't exist.");
+        }
+
+        $tempUser->delete();
+        return response()->json($tempUser, 200);
+    }
+}
